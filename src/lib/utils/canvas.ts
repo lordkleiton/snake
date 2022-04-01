2fc97e26cf3b6cedeceaff15257bdf6cd555fbf3
lord <m_s_r@outlook.com>
2022-04-01 01:17:42 -0300

    informações do canvas

--- src/lib/utils/canvas.ts
+++ src/lib/utils/canvas.ts
@@ -1,3 +1,4 @@
+import { ICanvasInfo } from "~/lib/interfaces";
 import ElementUtils from "./element";
 
 abstract class CanvasUtils {
@@ -7,6 +8,21 @@ abstract class CanvasUtils {
 
   static get context(): CanvasRenderingContext2D {
     return this.canvas.getContext("2d") || ({} as CanvasRenderingContext2D);
+  }
+
+  static get info(): ICanvasInfo {
+    return {
+      height: this.height,
+      width: this.width,
+    };
+  }
+
+  static get height(): number {
+    return this.canvas.height;
+  }
+
+  static get width(): number {
+    return this.canvas.width;
   }
 }
 
