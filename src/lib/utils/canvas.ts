dc2e3a1214f40bbdfd6c1c8637998f08ef0a55db
lord <m_s_r@outlook.com>
2022-04-01 01:49:34 -0300

    movendo metodos pra utils de canvas

--- src/lib/utils/canvas.ts
+++ src/lib/utils/canvas.ts
@@ -1,4 +1,5 @@
 import { ICanvasInfo } from "~/lib/interfaces";
+import { Snake } from "~/lib/models";
 import ElementUtils from "./element";
 
 abstract class CanvasUtils {
@@ -23,6 +24,31 @@ abstract class CanvasUtils {
 
   static get width(): number {
     return this.canvas.width;
+  }
+
+  static drawBackground(canvas_info: ICanvasInfo): void {
+    const lightest = ElementUtils.getCssVariableValue("--lightest");
+
+    this.context.fillStyle = lightest;
+
+    this.context.fillRect(0, 0, canvas_info.width, canvas_info.height);
+  }
+
+  static drawSnake(snake: Snake, size: number, half_block: number): void {
+    const darkest = ElementUtils.getCssVariableValue("--darkest");
+
+    this.context.fillStyle = darkest;
+
+    this.context.fillRect(
+      snake.x - half_block,
+      snake.y - half_block,
+      size,
+      size
+    );
+
+    this.context.fillStyle = "red";
+
+    this.context.fillRect(snake.x - 5, snake.y - 5, 10, 10);
   }
 }
 
