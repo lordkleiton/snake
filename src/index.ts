7487da3ed6d82df1fc80c2dcdca4c79adcd764d1
lord <m_s_r@outlook.com>
2022-04-01 02:02:40 -0300

    movendo logica de start do jogo pra gamedata

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,8 @@
-import {
-  CanvasUtils,
-  ElementUtils,
-  KeyboardUtils,
-  SnakeUtils,
-} from "~/lib/utils";
-import { Snake } from "~/lib/models";
+import { KeyboardUtils } from "~/lib/utils";
 import { GameData } from "~/lib/data";
-
-// canvas stuff
-const canvas_info = CanvasUtils.info;
-
-const instance = GameData.Instance;
 
 window.document.addEventListener("keydown", KeyboardUtils.keyboardHandler);
 
-// update screen
+const instance = GameData.Instance;
 
-const gameTick = () => {
-  const current_snake = SnakeUtils.handlePositioning(
-    snake,
-    half_block,
-    instance.snake_direction,
-    canvas_info
-  );
-
-  CanvasUtils.drawBackground(canvas_info);
-
-  CanvasUtils.drawSnake(current_snake, block_size, half_block);
-
-  snake = SnakeUtils.handleAcceleration(
-    current_snake,
-    instance.snake_direction,
-    movement_speed
-  );
-
-  requestAnimationFrame(gameTick);
-};
-
-requestAnimationFrame(gameTick);
-
-// outros
-
-const block_size = ElementUtils.remToPixels(1);
-const half_block = block_size / 2;
-const movement_speed = block_size / 10;
-
-let snake = new Snake(
-  canvas_info.width / 2 - half_block,
-  canvas_info.height / 2 - half_block
-);
+instance.startGame();
--- src/lib/data/game.ts
+++ src/lib/data/game.ts
@@ -1,14 +1,53 @@
 import { DirectionsEnum } from "~/lib/enums";
+import { Snake } from "~/lib/models";
+import { CanvasUtils, ElementUtils, SnakeUtils } from "~/lib/utils";
 
 class GameData {
   snake_direction: DirectionsEnum = DirectionsEnum.right;
 
   private static _instance: GameData;
+  private _snake: Snake;
+  private _canvas_info = CanvasUtils.info;
+  private _block_size = ElementUtils.remToPixels(1);
+  private _half_block = this._block_size / 2;
+  private _movement_speed = this._block_size / 10;
 
-  private constructor() {}
+  private constructor() {
+    const snake = new Snake(
+      this._canvas_info.width / 2 - this._half_block,
+      this._canvas_info.height / 2 - this._half_block
+    );
+
+    this._snake = snake;
+  }
 
   public static get Instance() {
     return this._instance || (this._instance = new this());
+  }
+
+  startGame() {
+    const gameTick = () => {
+      const current_snake = SnakeUtils.handlePositioning(
+        this._snake,
+        this._half_block,
+        this.snake_direction,
+        this._canvas_info
+      );
+
+      CanvasUtils.drawBackground(this._canvas_info);
+
+      CanvasUtils.drawSnake(current_snake, this._block_size, this._half_block);
+
+      this._snake = SnakeUtils.handleAcceleration(
+        current_snake,
+        this.snake_direction,
+        this._movement_speed
+      );
+
+      requestAnimationFrame(gameTick);
+    };
+
+    requestAnimationFrame(gameTick);
   }
 }
 
