96192a72130a8915f99e44ae3967e32784381360
lordkleiton <m_s_r@outlook.com>
2022-04-04 16:49:41 -0300

    unidades em 10px

--- src/lib/data/game_settings.ts
+++ src/lib/data/game_settings.ts
@@ -1,7 +1,7 @@
 import { ElementUtils } from "~/lib/utils";
 
 abstract class GameSettingsData {
-  static readonly block_size = ElementUtils.remToPixels(0.5);
+  static readonly block_size = ElementUtils.remToPixels(1);
   static readonly half_block = this.block_size / 2;
   static readonly movement_speed = this.block_size;
 }
