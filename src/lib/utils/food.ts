368d8c2ff0a89af97f5301ad68674e33d21611c5
lord <m_s_r@outlook.com>
2022-04-05 00:11:02 -0300

    checando se colisão ocorreu com comida

--- src/lib/utils/food.ts
+++ src/lib/utils/food.ts
@@ -1,3 +1,9 @@
-abstract class FoodUtils {}
+import { Food, SnakeSegment } from "~/lib/models";
+
+abstract class FoodUtils {
+  static collisionOccurred(food: Food, next_head: SnakeSegment): boolean {
+    return food.x == next_head.x && food.y == next_head.y;
+  }
+}
 
 export default FoodUtils;
