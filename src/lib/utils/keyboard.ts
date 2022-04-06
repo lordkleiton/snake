18bbd066f28c208d044592085df11280a0d62dc0
lordkleiton <m_s_r@outlook.com>
2022-04-06 16:23:23 -0300

    lendo atualizações do teclado apenas quando a anterior ja foi processada (desenhada na tela)

--- src/lib/utils/keyboard.ts
+++ src/lib/utils/keyboard.ts
@@ -2,8 +2,24 @@ import { GameData } from "~/lib/data";
 import { DirectionsEnum } from "~/lib/enums";
 
 abstract class KeyboardUtils {
+  private static _last_press: Date;
+
   static keyboardHandler = (event: KeyboardEvent) => {
+    const now = new Date();
+    const current_press_time = now.getTime();
     const { Instance: instance } = GameData;
+
+    if (!this._last_press) {
+      this._last_press = now;
+    }
+
+    const last_press_time = this._last_press.getTime();
+    const diff = current_press_time - last_press_time;
+
+    if (diff < instance.tick_time) return;
+
+    this._last_press = now;
+
     const { key } = event;
     const direction = instance.snake_direction;
     const down = "ArrowDown";
