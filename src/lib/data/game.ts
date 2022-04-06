57cc4482b0c6b22e4823b6893eb645adc40ca511
lordkleiton <m_s_r@outlook.com>
2022-04-06 16:22:54 -0300

    expondo o tick_time

--- src/lib/data/game.ts
+++ src/lib/data/game.ts
@@ -1,6 +1,6 @@
 import { DirectionsEnum } from "~/lib/enums";
 import { Snake, Food } from "~/lib/models";
-import { ElementUtils ,CanvasUtils, FoodUtils, SnakeUtils } from "~/lib/utils";
+import { ElementUtils, CanvasUtils, FoodUtils, SnakeUtils } from "~/lib/utils";
 import GameSettingsData from "./game_settings";
 
 class GameData {
@@ -9,6 +9,8 @@ class GameData {
   private static _instance: GameData;
   private _snake: Snake;
   private _food: Food;
+  private _tick_time: number = 100;
+  private _food_tracker: number = 0;
 
   private constructor() {
     const head = SnakeUtils.generateFirstHead();
@@ -22,9 +24,21 @@ class GameData {
     return this._instance || (this._instance = new this());
   }
 
+  public get tick_time(): number {
+    return this._tick_time;
+  }
+
+  private handleGameSpeed(): void {
+    this._food_tracker++;
+
+    if (this._food_tracker % 5 === 0 && this._tick_time > 30) {
+      this._tick_time -= 3;
+
+      console.log("gotta go fast: ", this._tick_time);
+    }
+  }
+
   startGame() {
-    var tick_time = 100;
-    var foodTracker = 0;
     const block_size = GameSettingsData.block_size;
 
     this._snake.addSegment(this.snake_direction, block_size);
@@ -51,14 +65,11 @@ class GameData {
       if (food_eaten) {
         SnakeUtils.eat(this._snake, this._food);
 
-        foodTracker++;
-        if (foodTracker % 5 === 0 && tick_time > 30) {
-          tick_time -= 3;
-          console.log("gotta go fast: ", tick_time);
-        }
-        ElementUtils.updateScore(foodTracker);
-        
-        this._food = FoodUtils.generateFood(this._snake);;
+        this.handleGameSpeed();
+
+        ElementUtils.updateScore(this._food_tracker);
+
+        this._food = FoodUtils.generateFood(this._snake);
       } else {
         SnakeUtils.updateSnake(this._snake, next_head);
       }
@@ -67,13 +78,14 @@ class GameData {
 
       if (game_over) {
         CanvasUtils.drawGameOver();
+
         return;
       }
 
-      setTimeout(gameTick, tick_time);
+      setTimeout(gameTick, this.tick_time);
     };
 
-    setTimeout(gameTick, tick_time);
+    setTimeout(gameTick, this.tick_time);
   }
 }
 
