a15a591eeca103b962e0d2f8f34e4953c7e50b14
lord <m_s_r@outlook.com>
2022-04-05 21:44:32 -0300

    util que pega quantas unidades do jogo um determinado tamnho possui

--- src/lib/utils/game_settings.ts
+++ src/lib/utils/game_settings.ts
@@ -1,8 +1,15 @@
 import { GameSettingsData } from "~/lib/data";
 
 abstract class GameSettingsUtils {
-  static getMiddleCoordinate(axis_size: number): number {
+  static getTotalUnits(axis_size: number): number {
     const units = axis_size / GameSettingsData.block_size;
+    const units_floor = Math.floor(units);
+
+    return units_floor;
+  }
+
+  static getMiddleCoordinate(axis_size: number): number {
+    const units = this.getTotalUnits(axis_size);
     const units_half = Math.floor(units / 2);
     const axis_half = units_half * GameSettingsData.block_size;
     const correct_position = axis_half - GameSettingsData.half_block;
